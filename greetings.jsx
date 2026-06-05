31c44012925f2bdb743d64d884b4ee1649bfda29
AbinayaVeeramani1803 <abinayaveeramani@thestackly.com>
2026-06-06 00:18:18 +0530

    greetings.jsx

---
+++ greetings.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Greetings() {
+  return(
+    <div>
+      <h3>Self introduction</h3>
+      {/* <p>
+        it, tenetur omnis eaque. Animi dolore soluta vero aliquid, hic molestiae nobis ratione ipsam ab eos dolor facilis quia harum cupiditate! Dolores aspernatur totam quis.
+      </p> */}
+      <p>
+       Name: Abinaya 
+       Education: Msc., Computer science
+       Skills: Teamwork
+       Location: Mannargudi
+       Hobbies: Reading books
+       Career Goal: Getting job
+      </p>
+    </div>
+  )
+}
