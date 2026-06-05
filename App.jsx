1c7a9f2dbe9b299fe10961ce83e970f4baebce02
AbinayaVeeramani1803 <abinayaveeramani@thestackly.com>
2026-06-06 00:16:56 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Greetings from './Greetings'
+
+export default function App() {
+
+  return(
+    <div>
+    
+      <h2>About me</h2>
+      <Greetings/>
+      
+      
+       </div>
+  )
+}
